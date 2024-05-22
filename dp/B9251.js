12a6c47dda7d79d4da9598fc3392e13aba05ceb0
djLee77 <dkdnl232@gmail.com>
2024-05-22 16:24:17 +0900

    (dp) : LCS

--- dp/B9251.js
+++ dp/B9251.js
@@ -1,20 +1,20 @@
 //LCS
 const fs = require('fs');
 
-const [A, B] = fs
-  .readFileSync('../input.txt')
+const [stringA, stringB] = fs
+  .readFileSync(0)
   .toString()
   .trim()
   .split('\n')
   .map((v) => v.replace(/\r/, '').split(''));
 
-let LCS = new Array(A.length + 1)
+let LCS = new Array(stringA.length + 1)
   .fill(0)
-  .map((v) => (v = new Array(B.length + 1).fill(0)));
+  .map((v) => (v = new Array(stringB.length + 1).fill(0)));
 
-for (let i = 1; i <= A.length; i++) {
-  for (let j = 1; j <= B.length; j++) {
-    if (A[i - 1] == B[j - 1]) {
+for (let i = 1; i <= stringA.length; i++) {
+  for (let j = 1; j <= stringB.length; j++) {
+    if (stringA[i - 1] == stringB[j - 1]) {
       LCS[i][j] = LCS[i - 1][j - 1] + 1;
     } else {
       LCS[i][j] = Math.max(LCS[i - 1][j], LCS[i][j - 1]);
@@ -22,15 +22,4 @@ for (let i = 1; i <= A.length; i++) {
   }
 }
 
-let str = '';
-let max = LCS[LCS.length - 1][LCS[LCS.length - 1].length - 1];
-for (let i = LCS[LCS.length - 1].length - 1; i > 0; i--) {
-  if (LCS[LCS.length - 1][i] === max) {
-    str = A[i - 1] + ' ' + str;
-    max--;
-  }
-}
-
-console.log(LCS);
-console.log(str);
-console.log(max);
+console.log(LCS.pop().pop());
