f02d72c22b1d5842a1643a7917aa9464453c6f7a
djLee77 <dkdnl232@gmail.com>
2024-06-03 22:34:03 +0900

    (divide-and-conquer) : 피보나치 수 6
    문제번호 11444

---
+++ divide-and-conquer/B11444.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const mod = 1000000007n;
+const N = BigInt(fs.readFileSync('../input.txt').toString().trim());
+let memo = new Map();
+
+const fibo = (N) => {
+  if (N === 0n) return 0n;
+  if (N === 1n) return 1n;
+  if (N === 2n) return 1n;
+  if (memo[N] > 0n) return memo[N];
+
+  if (N % 2n === 0n) {
+    memo[N] = (fibo(N / 2n) * (fibo(N / 2n + 1n) + fibo(N / 2n - 1n))) % mod;
+  }
+
+  if (N % 2n === 1n) {
+    memo[N] = (fibo((N + 1n) / 2n) ** 2n + fibo((N - 1n) / 2n) ** 2n) % mod;
+  }
+
+  return memo[N];
+};
+
+console.log(fibo(N).toString().replace('n', ''));
